f51a610ceef11064f74bf4b0118dde519ad3f328
labiblianoshablapodcast-gif <labiblianoshablapodcast@gmail.com>
2026-09-18 09:26:39 -0400

    Build Gradivo Keys Chapter 3 Roman degrees

---
+++ src/degrees.ts
@@ -0,0 +1,4 @@
+export const DEGREE_LABELS=['I','ii','iii','IV','V','vi','vii°','I'] as const;
+export const DEGREE_QUALITY=['Mayor','menor','menor','Mayor','Mayor','menor','disminuido','Mayor'] as const;
+export const C_MAJOR=['C','D','E','F','G','A','B','C'] as const;
+export const D_MAJOR=['D','E','F#','G','A','B','C#','D'] as const;
